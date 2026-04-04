3392a5fc382bf408e121fb568c245bfbb2f89400
f4209138-ctrl <f4209138@gmail.com>
2026-04-04 21:44:03 +0530

    main

---
+++ ACP/lesson 5.py
@@ -0,0 +1,5 @@
+temp = float(input("Please check and mention the temprature outside in decimal:"))
+if temp > 25:
+    print("You should wear cool clothes like half sleve shirt or t-shirt.")
+else:
+    print("You should wear warm clothes like sweater and full lowers.")
