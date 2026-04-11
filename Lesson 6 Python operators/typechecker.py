97746d48c87e536dd7c4520d852891361e7e4f7f
f4209138-ctrl <f4209138@gmail.com>
2026-04-11 21:21:57 +0530

    main

---
+++ Lesson 6 Python operators/typechecker.py
@@ -0,0 +1,16 @@
+a  = 5
+if (type(a) is int):
+    print("It is a integer.")
+else:
+    print("It is not a integer")
+x = 5.5
+if (type(x) is not float):
+    print("True")
+else:
+    print("false")
+x = 20 
+y = 20
+if (x is y):
+    print("20 is equal to 20")
+else:
+    print("20 is not equal to 20")
