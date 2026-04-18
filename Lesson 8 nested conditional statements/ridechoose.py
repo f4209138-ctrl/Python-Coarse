29a28ca7537280bc04a55fe82d4338d7b7981f8e
f4209138-ctrl <f4209138@gmail.com>
2026-04-18 21:38:50 +0530

    main

---
+++ Lesson 8 nested conditional statements/ridechoose.py
@@ -0,0 +1,6 @@
+choice=int(input("Which ride do you want to choose 1 = bike , option 2 = car:"))
+if choice == 1:
+   choice2 = int(input("Which Ride do you want to take scooty or scooter 1 and 2 respectively:"))
+else:
+    choice2 = int(input("Which car do you want to choose Xuv or a Sedan 1 and 2 respectively:"))
+print("You choosed", choice2,",Excelent choice")
