ea58b4392b0936ff15ae999c7487943b4e81783a
f4209138-ctrl <f4209138@gmail.com>
2026-04-19 15:21:53 +0530

    main

---
+++ Lesson 9 Loops/reversestring.py
@@ -0,0 +1,6 @@
+string = input("Please enter a word to be reversed:")
+string2 = ('')
+for i in string:
+    string2 = i +string2
+print("The original word was:",string)
+print("The reversed word is:",string2)
