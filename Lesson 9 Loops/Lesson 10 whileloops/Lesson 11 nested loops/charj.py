b0cd90aa0144e0dd572357f7491d82648b0606e8
f4209138-ctrl <f4209138@gmail.com>
2026-04-26 15:11:44 +0530

    main

---
+++ Lesson 9 Loops/Lesson 10 whileloops/Lesson 11 nested loops/charj.py
@@ -0,0 +1,9 @@
+string  = input("Please enter a word:")
+char = input("PLease enter a charecter:")
+i = 0
+count=0
+while(i<len(string)):
+    if(string[i]==char):
+        count = count +1
+    i = i+1
+    print("The total number of times", char, "has occured:", count)
