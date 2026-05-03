102bb73807deb877ef1883c2645ac9a2e0a42593
f4209138-ctrl <f4209138@gmail.com>
2026-05-03 15:16:29 +0530

    main

---
+++ Lesson 9 Loops/lesson 11 turtle/project.py
@@ -0,0 +1,11 @@
+import turtle 
+turtle.Screen().bgcolor("green")
+turtle.Screen().setup(300,400)
+polygon = turtle.Turtle()
+num_side= 6
+side_length= 70
+angle= 360.0 / num_side
+for i in range(num_side):
+    polygon.forward(side_length)
+    polygon.right(angle)
+turtle.done()
