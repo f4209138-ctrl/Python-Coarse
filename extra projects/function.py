a2c181f1574c530c12473f5149a345cf0bb0443e
f4209138-ctrl <f4209138@gmail.com>
2026-05-10 15:48:02 +0530

    main

---
+++ extra projects/function.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def calculate_both(a,b):
+    sum_val= add(a,b)
+    diff_val= add(a,b)
+    print(f"Addition:{sum_val}")
+    print(f"Subtraction:{diff_val}")
+calculate_both(10,5)
+
