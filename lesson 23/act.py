c9e2c4d9471e4e62805f410675e3c6b871525c2c
f4209138-ctrl <f4209138@gmail.com>
2026-06-06 21:14:01 +0530

    main

---
+++ lesson 23/act.py
@@ -0,0 +1,11 @@
+tuplex=("tuple", False,3.2,1)
+print(tuplex)
+tuplex = tuplex +(9,)
+print(tuplex)
+tuple1=(50,10,60,70,50)
+print(tuple1.count(50))
+tuplex =(50,10,60,70,50)
+_slice=tuplex[3:5]
+print(_slice)
+_slice = tuplex[:6]
+print(_slice)
