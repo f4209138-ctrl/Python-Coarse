47740f6999fcc90a7f4b430c640cdc04a2397ad5
f4209138-ctrl <f4209138@gmail.com>
2026-06-07 14:38:28 +0530

    main

---
+++ lesson 24/actsiudopasjdu3.py
@@ -0,0 +1,7 @@
+country_code ={'India':'0091',
+               'Austrailia':'0011',
+               'Nepal':'00977'}
+print("Country code for india is")
+print(country_code.get('India','not found'))
+print("Country code for Japan is")
+print(country_code.get('japan','not found'))
