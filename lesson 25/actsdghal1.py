185d4f98fd4b9a2d8429fdadd6e5cf8e5648e600
f4209138-ctrl <f4209138@gmail.com>
2026-06-14 15:19:17 +0530

    main

---
+++ lesson 25/actsdghal1.py
@@ -0,0 +1,15 @@
+my_set={1,2,3}
+print(my_set)
+my_set = {1.0,"Hello",(1,2,3)}
+print(my_set)
+my_set = {1,2,3,4,3,2}
+print(my_set)
+my_set = set([1,2,3,2])
+print(my_set,"\n")
+num_set =set([0,1,3,4,5])
+print("ORiginalset:")
+print(num_set)
+num_set.pop()
+print("After removing the pop it would be:")
+print(num_set,"\n")
+
