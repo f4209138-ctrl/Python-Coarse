7d695c488572cf59166160a4a670646071b0ace3
f4209138-ctrl <f4209138@gmail.com>
2026-06-20 21:22:34 +0530

    main

---
+++ lesson 26/lesson 26.py
@@ -0,0 +1,7 @@
+list1= [1,2,3]
+list2=[4,5,6,]
+sum_list=list(map(lambda x,y:x+y,list1,list2))
+print(sum_list)
+source_list=[1,2,3,4,5]
+square_list=list(map(lambda x:x**2,source_list))
+print(square_list)
