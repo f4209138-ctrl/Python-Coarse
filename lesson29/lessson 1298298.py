563480bb3fcfd716727fa60f02333f97d4dad7e9
f4209138-ctrl <f4209138@gmail.com>
2026-06-27 21:26:13 +0530

    main

---
+++ lesson29/lessson 1298298.py
@@ -0,0 +1,9 @@
+class Vehicles:
+
+
+    def __init__(self,max_speed,mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+modelX=Vehicles(240,18)
+print(modelX.mileage)
+print(modelX.max_speed)
