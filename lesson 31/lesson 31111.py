d99dfb5de291224fac50c44f51e30dfcf549f9c8
f4209138-ctrl <f4209138@gmail.com>
2026-07-04 21:23:12 +0530

    main

---
+++ lesson 31/lesson 31111.py
@@ -0,0 +1,9 @@
+class Vehicle:
+    def __init__(self,name,max_speed,mileage):
+        self.name = name
+        self.max_speed= max_speed
+        self.mileage = mileage
+class Bus(Vehicle):
+    pass
+School_bus=Bus("school volvo",180,12)
+print("Vehicle name:",School_bus.name, School_bus.max_speed, School_bus.mileage)
