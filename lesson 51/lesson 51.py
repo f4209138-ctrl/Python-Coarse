2dc596cea08d08543f7e6eb0497b6e2abc0dc924
f4209138-ctrl <f4209138@gmail.com>
2026-08-30 15:02:37 +0530

    main

--- lesson 51/lesson 51.py
+++ lesson 51/lesson 51.py
@@ -1,43 +1,44 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-window=Tk()
+
+window = Tk()
 window.title("Not Codingal's text editor")
 window.geometry("600x600")
-window.rowconfigure(0,minsize=800,weight=1)
-window.columnconfigure(1,minsize=800,weight=1)
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
 def open_file():
-    """Open a file for editing"""
-    filepath=askopenfilename(
-        filetypes=[("Text files","*,.txt"),("All files","*.*")]
+    filepath = askopenfilename(
+        filetypes=[("Text files", "*.txt"), ("All files", "*.*")]
     )
     if not filepath:
         return
-    txt_edit.delete(1.0,END)
-    with open(filepath,"r") as input_file:
-        text=input_file.read()
-        txt_edit.insert(END,text)
-        input_file.close()
+    txt_edit.delete(1.0, END)
+    with open(filepath, "r") as input_file:
+        text = input_file.read()
+        txt_edit.insert(END, text)
     window.title(f"Not Codingal's Text Editor-{filepath}")
+
 def save_file():
-    filepath=asksaveasfilename(
-    defaultextension="txt",
-    filetypes=[("Text Files","*.txt"),("All files","*.*")],
+    filepath = asksaveasfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All files", "*.*")],
     )
     if not filepath:
-      return
-    with open(filepath,"w") as output_file:
-      text=txt_edit.get(1.0,END)
-      output_file.write(text)
+        return
+    with open(filepath, "w") as output_file:
+        text = txt_edit.get(1.0, END)
+        output_file.write(text)
     window.title(f"Not Codingal's text editor-{filepath}")
-txt_edit=Text(window)
-fr_buttons=Frame(window,relief=RAISED,bd=2)
-btn_open=Button(fr_buttons,text="Open",command=open_file)
-btn_save=Button(fr_buttons,text="SAVE as",command=save_file)
-btn_open.grid(row=0,column=0,sticky="ew",padx=5,pady=5)
-btn_save.grid(row=0,column=0,sticky="ew",padx=5)
-fr_buttons.grid(row=0,column=0,sticky="ns")
-txt_edit.grid(row=0,column=1,sticky="nsew")
+
+txt_edit = Text(window)
+fr_buttons = Frame(window, relief=RAISED, bd=2)
+btn_open = Button(fr_buttons, text="Open", command=open_file)
+btn_save = Button(fr_buttons, text="SAVE as", command=save_file)
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+fr_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
 window.mainloop()
-
-
-
